18e5b4fd5010b5840269795baae205e5b87ab64d
noop <npineiro9@gmail.com>
2023-12-31 04:16:24 +0100

    Add first dialog

--- js/hooks.js
+++ js/hooks.js
@@ -7,5 +7,15 @@ Hooks.once("ready", function() {
 });
 
 Hooks.on("aedas-mass-combat-build-army", function() {
-    console.log(game.user.isGM);
+    if (game.user.isGM) {
+        let actorObj = game.actors.get(game.tokenActionHUD.getData().actions.actorId);
+        if (actorObj === undefined) {
+            ui.notifications.error("Error at execution. No TokenActionHUD found to retrieve active Actor ID.");
+            return;
+        }
+        bs = new BaseSheet(actorObj, {template: 'modules/aedas-mass-combat/template/buildarmy.html'});
+        bs.render(true);
+    } else {
+        ui.notifications.error("You are not a GM.");
+    }
 });
